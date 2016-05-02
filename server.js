edddb6ef10f2cab3a6f1fa288d95452664748ab3
Kevin Chen <chen884@usc.edu>
2016-05-02 00:52:33 -0700

    server

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.use(function (request, response, next) {
     next();
 });
 
+app.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', http.address().port);
+});
+
 // app.listen(8000, function(){
 // 	console.log("Listening on port 8000");
 // });
