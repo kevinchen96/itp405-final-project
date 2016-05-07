0437f2d27cc36be51ec1a1cf1663b274c2766137
Kevin Chen <chen884@usc.edu>
2016-05-06 23:45:20 -0700

    changes to fix dx deploy

--- dist/assets/itp405-final-project.js
+++ dist/assets/itp405-final-project.js
@@ -9886,7 +9886,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("itp405-final-project/app")["default"].create({"name":"itp405-final-project","version":"0.0.0+bfa67aca"});
+  require("itp405-final-project/app")["default"].create({"name":"itp405-final-project","version":"0.0.0+ada3f491"});
 }
 
 /* jshint ignore:end */
